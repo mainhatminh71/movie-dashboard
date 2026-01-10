d7b7d58f78613f8d21071b92b373aa4711106767
Mai Nhật Minh <nhatmoon71@gmail.com>
2026-01-10 15:31:55 +0700

    Update environment for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 /* import { SecretEnvironment } from "./environment.secret"
  */export const environment = {
-    production: false,
+    production: true,
     tmdbApiKey: /* SecretEnvironment.tmdbApiKey || */ 'TMDB_API_KEY',
     tmdbBaseUrl: 'https://api.themoviedb.org/3',
     tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/'
