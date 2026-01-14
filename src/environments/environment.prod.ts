45cf7c03d9fa547c18fb8223476b1017c4b0b2b8
Mai Nhật Minh <nhatmoon71@gmail.com>
2026-01-14 16:47:10 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,9 @@ export const environment = {
   tmdbApiKey: 'TMDB_API_KEY',
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/',
-  tmdbApiToken: /* SecretEnvironment.tmdbApiToken || */ 'TMDB_API_TOKEN'
+  tmdbApiToken: /* SecretEnvironment.tmdbApiToken || */ 'TMDB_API_TOKEN',
+  groqApiKey: 'GROQ_API_KEY',
+  groqBaseUrl: 'https://api.groq.com/openai/v1',
+  openaiApiKey: 'OPENAI_API_KEY'
 };
 
